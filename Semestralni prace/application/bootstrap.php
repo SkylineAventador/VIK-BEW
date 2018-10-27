f28d24fb41dacc3e39cd992abd832e077a2999a8
Kravtsov Dmytro <kravtsov.d@seznam.cz>
2018-10-28 00:02:25 +0200

    Correcting URLs processing.
    Signed-off-by: Kravtsov Dmytro <kravtsov.d@seznam.cz>

--- Semestralni prace/application/bootstrap.php
+++ Semestralni prace/application/bootstrap.php
@@ -5,3 +5,8 @@
  * Date: 27.10.2018
  * Time: 23:22
  */
+require_once 'core/model.php';
+require_once 'core/view.php';
+require_once 'core/controller.php';
+require_once 'core/route.php';
+Route::start();
--- Semestralni prace/index.php
+++ Semestralni prace/index.php
@@ -5,3 +5,5 @@
  * Date: 27.10.2018
  * Time: 23:23
  */
+ini_set('display_errors', 1);
+require_once 'application/bootstrap.php';
