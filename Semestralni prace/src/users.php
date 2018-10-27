34c43a06c6bbe40bc17e27f14d14b10eee179821
Kravtsov Dmytro <kravtsov.d@seznam.cz>
2018-10-27 14:10:01 +0200

    Structure service. Locale Bootstrap connected. FontAwesome icons added. MVC architecture establised.
    Signed-off-by: Kravtsov Dmytro <kravtsov.d@seznam.cz>

--- Semestralni prace/src/users.php
+++ Semestralni prace/src/users.php
@@ -1,26 +1,23 @@
 <!DOCTYPE html>
 <html lang="cs">
 <head>
-    <meta charset="UTF-8">
     <title>Uzivatele</title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/mainOwnStyles.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <!-- Proper bootstrap elements scaling -->
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/mainOwnStyles.css">
+    <!--FontAwesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css"
+          integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns"
+          crossorigin="anonymous">
+    <script src="../js/bootstrap.min.js"></script>
 </head>
 <body>
 
 <div class="gBox usersBg pb-1">
-    <header>
-        <h1 class="mb-3 text-center">Uzivatele konferencniho systemu</h1>
-    </header>
+<!--    <header>-->
+<!--        <h1 class="mb-3 text-center">Uzivatele konferencniho systemu</h1>-->
+<!--    </header>-->
 
     <div class="container border shadow-lg usrBgSq rounded">
         <?php
@@ -45,7 +42,7 @@
 <?php
     function cUsrCard($userName, $userDesc){
         $returnString = "<div class=\"card userCard bg-light text-dark shadow\">
-            <img class=\"card-img-top\" src=\"../img/unknown_user_img.png\" alt=\"Unknown user\"/>
+            <img class=\"card-img-top\" src=\"../Images/unknown_user_img.png\" alt=\"Unknown user\"/>
             <div class=\"card-body\">
                 <h4 class=\"card-title\">$userName</h4>
                 <p class=\"card-text\">$userDesc</p>
